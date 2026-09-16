ed9b830f594732a713d6a4ec2fa02a9d4fadb30a
mrudula-girireddy <mrudula0870@gmail.com>
2026-09-16 13:03:39 -0400

    Task-2323

--- src/main/java/com/trainerdemo/customer/controller/ApiExceptionHandler.java
+++ src/main/java/com/trainerdemo/customer/controller/ApiExceptionHandler.java
@@ -13,6 +13,7 @@ public class ApiExceptionHandler {
 
     @ExceptionHandler(CustomerNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleCustomerNotFound(CustomerNotFoundException exception) {
+       System.out.println("Task-2323");
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(Map.of("message", exception.getMessage()));
