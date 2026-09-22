f6eb4fb5ac92f07b6c754e0223f32ce8ee34daca
mrudula-girireddy <mrudula0870@gmail.com>
2026-09-22 11:41:39 -0400

    Demo trainer update customer endpoint comment

--- src/main/java/com/trainerdemo/customer/controller/CustomerController.java
+++ src/main/java/com/trainerdemo/customer/controller/CustomerController.java
@@ -18,7 +18,7 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/{id}") // Fetch customer by customer id
     public ResponseEntity<CustomerResponse> getCustomerById(@PathVariable Long id) {
         return ResponseEntity.ok(customerService.getCustomerById(id));
     }
